ac4dba00320aa134e698e59dd01b26c6c09ff3e6
mkumbobeaty <charsbeaty@gmail.com>
2019-01-26 11:46:50 +0300

    feat(assessments): add new layout
    create questions layout under assessment module

---
+++ src/Assessments/components/Questions/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.css';
+
+/**
+ * Render question list which have search box, actions and question list
+ *
+ * @class
+ * @name QuestionsList
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export default function QuestionsList() {
+  return <h2>question list</h2>;
+}
