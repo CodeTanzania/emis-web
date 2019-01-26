c3b8958d9b86e3efef115e0acfaed3fd98581b5e
mloweedgar <mloweedgar0@gmail.com>
2019-01-26 10:41:27 +0300

    feat(resources): add warehouses layout
    add layout to display warehouses

--- src/GeographicalFeatures/layouts/Warehouses.js
+++ src/GeographicalFeatures/layouts/Warehouses.js
@@ -1,7 +1,15 @@
 import React from 'react';
-// import UIState from '../../components/UIState';
-import WarehouseList from '../components/Warehouses';
+import UIState from '../../components/UIState';
 
-const WarehousesLayout = () => <WarehouseList />;
+const WarehousesLayout = () => (
+  <div style={{ marginTop: '20%' }}>
+    <UIState
+      icon="exclamation-circle"
+      description="No Alerts Actions yet,but when they are available will appear here"
+      buttonLabel="New Alert"
+      onClick={() => {}}
+    />
+  </div>
+);
 
 export default WarehousesLayout;
