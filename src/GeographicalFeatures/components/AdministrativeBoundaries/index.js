1b8541c7913d886b81bb10147bc7da6b14e92d04
mloweedgar <mloweedgar0@gmail.com>
2019-01-28 17:17:19 +0300

    feat(geographical features): display filter and header row metadata

--- src/GeographicalFeatures/components/AdministrativeBoundaries/index.js
+++ src/GeographicalFeatures/components/AdministrativeBoundaries/index.js
@@ -1,24 +1,29 @@
 import { Connect, getFeatures } from '@codetanzania/emis-api-states';
-import { Input, List, Col, Row } from 'antd';
+import { Input, Modal, Col, Row } from 'antd';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import AdminstrativeBoundaryListItem from './ListItem';
+import AdminstrativeBoundaryList from './List';
+import AdminstrativeBoundaryFilters from './Filters';
 import AdminstrativeBoundariesActionBar from './ActionBar';
 import './styles.css';
 
 const { Search } = Input;
 
 /**
- * Render adminstrative boundarie list which have search box and actions
+ * Render Adminstrative Boundary module which has search box, actions and list of Adminstrative Boundaries
  *
  * @class
- * @name AdminstrativeBoundaryList
+ * @name AdminstrativeBoundary
  *
  *
  * @version 0.1.0
  * @since 0.1.0
  */
-class AdminstrativeBoundaryList extends Component {
+class AdminstrativeBoundaries extends Component {
+  state = {
+    showFilters: false,
+  };
+
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     adminstrativeBoundaries: PropTypes.arrayOf(
@@ -35,17 +40,64 @@ class AdminstrativeBoundaryList extends Component {
     getFeatures();
   }
 
+  /**
+   * open filters modal by setting it's visible property to false via state
+   *
+   * @function
+   * @name openFiltersModal
+   *
+   * @returns {undefined} - Nothing is returned
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  openFiltersModal = () => {
+    this.setState({ showFilters: true });
+  };
+
+  /**
+   * Close filters modal by setting it's visible property to false via state
+   *
+   * @function
+   * @name closeFiltersModal
+   *
+   * @returns {undefined} - Nothing is returned
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  closeFiltersModal = () => {
+    this.setState({ showFilters: false });
+  };
+
+  /**
+   * Search AdminstrativeBoundaries List based on supplied filter word
+   *
+   * @function
+   * @name searchAdminstrativeBoundaries
+   *
+   * @param {Object} event - Event instance
+   * @returns {undefined} - Nothing is returned
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  searchAdminstrativeBoundaries = event => {
+    getFeatures({ q: event.target.value });
+  };
+
   render() {
     const { adminstrativeBoundaries, loading, total, page } = this.props;
+    const { showFilters } = this.state;
     return (
-      <div className="AdminstrativeBoundaryList">
+      <div className="AdminstrativeBoundaries">
         <Row>
           <Col span={12}>
             {/* search input component */}
             <Search
               size="large"
               placeholder="Search for adminstrative boundaries here here ..."
-              onChange={({ target: { value } }) => getFeatures({ q: value })}
+              onChange={this.searchAdminstrativeBoundaries}
             />
             {/* end search input component */}
           </Col>
@@ -54,27 +106,32 @@ class AdminstrativeBoundaryList extends Component {
           {/* end primary actions */}
         </Row>
         {/* list action bar */}
-        <AdminstrativeBoundariesActionBar total={total} page={page} />
+        <AdminstrativeBoundariesActionBar
+          total={total}
+          page={page}
+          onFilter={this.openFiltersModal}
+        />
         {/* end list action bar */}
         {/* list starts */}
-        <List
+        <AdminstrativeBoundaryList
+          adminstrativeBoundaries={adminstrativeBoundaries}
           loading={loading}
-          dataSource={adminstrativeBoundaries}
-          renderItem={adminstrativeBoundary => (
-            <AdminstrativeBoundaryListItem
-              key={adminstrativeBoundary.name}
-              name={adminstrativeBoundary.name}
-              level={adminstrativeBoundary.level}
-            />
-          )}
         />
         {/* end list */}
+        <Modal
+          title="Filter AdminstrativeBoundaries"
+          visible={showFilters}
+          onCancel={this.closeFiltersModal}
+          footer={null}
+        >
+          <AdminstrativeBoundaryFilters onCancel={this.closeFiltersModal} />
+        </Modal>
       </div>
     );
   }
 }
 
-export default Connect(AdminstrativeBoundaryList, {
+export default Connect(AdminstrativeBoundaries, {
   adminstrativeBoundaries: 'features.list',
   loading: 'features.loading',
   page: 'features.page',
