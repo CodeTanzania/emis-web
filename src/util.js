f7f1711e4bdf3d1c3a9d45603c3d6c788e9eaa83
Benson Maruchu <benmaruchu@gmail.com>
2019-01-29 12:06:56 +0300

    feat(util): add antd message wrapper functions

---
+++ src/util.js
@@ -0,0 +1,53 @@
+import { message } from 'antd';
+
+/**
+ * Show error message box
+ *
+ * @function
+ * @name notifyError
+ *
+ * @param {Object} error
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export const notifyError = error => {
+  // eslint-disable-next-line
+  if (!navigator.onLine) {
+    return message.error(
+      'You are currently offline, Please ensure Network connection is available'
+    );
+  }
+
+  return message.error(error.message);
+};
+
+/**
+ * Show a success message box
+ *
+ * @function
+ * @name notifySuccess
+ *
+ * @param {string} details
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export const notifySuccess = details => {
+  message.success(details);
+};
+
+/**
+ * Show a info message box
+ *
+ * @function
+ * @name notifyInfo
+ *
+ * @param {string} details
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export const notifyInfo = info => {
+  message.info(info);
+};
