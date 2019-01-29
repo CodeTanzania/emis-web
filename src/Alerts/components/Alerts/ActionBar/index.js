3c33e28d9e0b5cc2b426d105f3b6d67382fdf951
mloweedgar <mloweedgar0@gmail.com>
2019-01-29 18:01:39 +0300

    feat(alerts): add action bar for issued alerts

---
+++ src/Alerts/components/Alerts/ActionBar/index.js
@@ -0,0 +1,124 @@
+import { getAlerts } from '@codetanzania/emis-api-states';
+import { Button, Checkbox, Col, Pagination, Row } from 'antd';
+import PropTypes from 'prop-types';
+import React from 'react';
+import './styles.css';
+
+/**
+ * Render action bar for actions which are applicable to list content
+ *
+ * @function
+ * @name AlertsActionBar
+ *
+ * @param {Object} props
+ * @param {page} props.page
+ * @param {number} props.total
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+const AlertsActionBar = ({ page, total, onFilter }) => (
+  <div className="AlertsActionBar">
+    <Row>
+      <Col span={1} xl={1} className="checkbox">
+        <Checkbox />
+      </Col>
+
+      <Col span={1} xl={1}>
+        <Button
+          shape="circle"
+          icon="reload"
+          title="Refresh Alerts"
+          onClick={() => getAlerts()}
+          className="actionButton"
+          size="large"
+        />
+      </Col>
+
+      <Col span={1} xl={1}>
+        <Button
+          type="circle"
+          icon="mail"
+          title="Send Alerts as Email"
+          className="actionButton"
+          size="large"
+        />
+      </Col>
+
+      <Col span={1} xl={1}>
+        <Button
+          type="circle"
+          icon="message"
+          title="Send Alerts as SMS"
+          className="actionButton"
+          size="large"
+        />
+      </Col>
+
+      <Col span={1} xl={1}>
+        <Button
+          type="circle"
+          icon="cloud-download"
+          title="Export selected Alerts"
+          className="actionButton"
+          size="large"
+        />
+      </Col>
+
+      <Col span={1} xl={1}>
+        <Button
+          type="circle"
+          icon="share-alt"
+          title="Share selected Alerts"
+          className="actionButton"
+          size="large"
+        />
+      </Col>
+
+      <Col span={1} xl={1}>
+        <Button
+          type="circle"
+          icon="hdd"
+          title="Archive selected Alerts"
+          className="actionButton"
+          size="large"
+        />
+      </Col>
+
+      <Col
+        span={1}
+        offset={13}
+        xl={{ span: 1, offset: 12 }}
+        xxl={{ span: 1, offset: 13 }}
+      >
+        <Button
+          type="circle"
+          icon="filter"
+          title="Filter Alerts"
+          className="actionButton"
+          size="large"
+          onClick={onFilter}
+        />
+      </Col>
+
+      <Col span={3} xl={4} xxl={3}>
+        <Pagination
+          simple
+          defaultCurrent={page}
+          total={total}
+          onChange={nextPage => getAlerts({ page: nextPage })}
+          className="pagination"
+        />
+      </Col>
+    </Row>
+  </div>
+);
+
+/* props validation */
+AlertsActionBar.propTypes = {
+  page: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  onFilter: PropTypes.func.isRequired,
+};
+
+export default AlertsActionBar;
