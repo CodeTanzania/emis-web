ea14fffde5775e0f0593e97cf1758a197b12c52b
Benson Maruchu <benmaruchu@gmail.com>
2019-01-30 20:22:41 +0300

    feat(stakeholders): add notification form

--- src/Stakeholders/components/Contacts/ActionBar/index.js
+++ src/Stakeholders/components/Contacts/ActionBar/index.js
@@ -21,7 +21,13 @@ import './styles.css';
  * @version 0.1.0
  * @since 0.1.0
  */
-const ContactsActionBar = ({ page, total, onFilter }) => (
+const ContactsActionBar = ({
+  page,
+  total,
+  selectedCount,
+  onFilter,
+  onNotify,
+}) => (
   <div className="ContactsActionBar">
     <Row>
       <Col span={1} xl={1} className="checkbox">
@@ -54,57 +60,64 @@ const ContactsActionBar = ({ page, total, onFilter }) => (
         <Button
           type="circle"
           icon="mail"
-          title="Send Email to selected contacts"
+          title={`Send Notification to${
+            selectedCount > 0 ? ' selected' : ''
+          } contacts`}
           className="actionButton"
           size="large"
+          onClick={onNotify}
         />
       </Col>
 
       <Col span={1} xl={1}>
-        <Button
-          type="circle"
-          icon="message"
-          title="Send SMS to selected contacts"
-          className="actionButton"
-          size="large"
-        />
+        {selectedCount > 0 && (
+          <Button
+            type="circle"
+            icon="cloud-download"
+            title="Export selected contacts"
+            className="actionButton"
+            size="large"
+          />
+        )}
       </Col>
 
       <Col span={1} xl={1}>
-        <Button
-          type="circle"
-          icon="cloud-download"
-          title="Export selected contacts"
-          className="actionButton"
-          size="large"
-        />
+        {selectedCount > 0 && (
+          <Button
+            type="circle"
+            icon="share-alt"
+            title="Share selected contacts"
+            className="actionButton"
+            size="large"
+          />
+        )}
       </Col>
 
       <Col span={1} xl={1}>
-        <Button
-          type="circle"
-          icon="share-alt"
-          title="Share selected contacts"
-          className="actionButton"
-          size="large"
-        />
-      </Col>
-
-      <Col span={1} xl={1}>
-        <Button
-          type="circle"
-          icon="hdd"
-          title="Archive selected contacts"
-          className="actionButton"
-          size="large"
-        />
+        {selectedCount > 0 && (
+          <Button
+            type="circle"
+            icon="hdd"
+            title="Archive selected contacts"
+            className="actionButton"
+            size="large"
+          />
+        )}
       </Col>
 
       <Col
+        span={3}
+        offset={7}
+        xl={{ span: 3, offset: 7 }}
+        xxl={{ span: 3, offset: 7 }}
+      >
+        {selectedCount > 0 && <h4>{`${selectedCount} Contact(s) Selected`}</h4>}
+      </Col>
+      <Col
         span={1}
-        offset={13}
-        xl={{ span: 1, offset: 12 }}
-        xxl={{ span: 1, offset: 13 }}
+        offset={4}
+        xl={{ span: 1, offset: 3 }}
+        xxl={{ span: 1, offset: 4 }}
       >
         <Button
           type="circle"
@@ -133,7 +146,9 @@ const ContactsActionBar = ({ page, total, onFilter }) => (
 ContactsActionBar.propTypes = {
   page: PropTypes.number.isRequired,
   total: PropTypes.number.isRequired,
+  selectedCount: PropTypes.number.isRequired,
   onFilter: PropTypes.func.isRequired,
+  onNotify: PropTypes.func.isRequired,
 };
 
 export default ContactsActionBar;
