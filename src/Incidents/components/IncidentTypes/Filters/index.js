7887850905fc5f1181207a7d9bef490e0aa70874
mkumbobeaty <charsbeaty@gmail.com>
2019-01-31 12:49:48 +0300

    refactor(incidents): rearrange filter buttons

--- src/Incidents/components/IncidentTypes/Filters/index.js
+++ src/Incidents/components/IncidentTypes/Filters/index.js
@@ -138,14 +138,12 @@ class IncidentTypesFilters extends Component {
         {/* end nature filters */}
         {/* form actions */}
         <Form.Item wrapperCol={{ span: 24 }} style={{ textAlign: 'right' }}>
-          <Button type="primary" htmlType="submit">
-            Filter
-          </Button>
+          <Button onClick={onCancel}>Cancel</Button>
           <Button style={{ marginLeft: 8 }} onClick={this.handleClearFilter}>
             Clear
           </Button>
-          <Button style={{ marginLeft: 8 }} onClick={onCancel}>
-            Cancel
+          <Button type="primary" htmlType="submit" style={{ marginLeft: 8 }}>
+            Filter
           </Button>
         </Form.Item>
         {/* end form actions */}
