8bb61246cc0cd40d9a649ef0eba1085aa1ce05c3
Benson Maruchu <benmaruchu@gmail.com>
2019-02-04 09:07:55 +0300

    feat(emergency plans): add ability to create and edit plan

--- src/Plans/components/Planner/PlanForm/index.js
+++ src/Plans/components/Planner/PlanForm/index.js
@@ -1,7 +1,14 @@
+import {
+  getFeatures,
+  getIncidentTypes,
+  getStakeholders,
+} from '@codetanzania/emis-api-client';
 import { postPlan, putPlan } from '@codetanzania/emis-api-states';
-import { Button, Form, Input } from 'antd';
+import { Button, Form } from 'antd';
+import upperFirst from 'lodash/upperFirst';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import SearchableSelectInput from '../../../../components/SearchableSelectInput';
 import { notifyError, notifySuccess } from '../../../../util';
 
 /**
@@ -108,55 +115,73 @@ class PlanForm extends Component {
 
     return (
       <Form onSubmit={this.handleSubmit} autoComplete="off">
-        {/* plan name */}
-        <Form.Item {...formItemLayout} label="Full Name">
-          {getFieldDecorator('name', {
-            initialValue: isEditForm ? plan.name : undefined,
-            rules: [{ required: true, message: 'Plan full name is required' }],
-          })(<Input placeholder="e.g John Doe" />)}
-        </Form.Item>
-        {/* end plan name */}
-
-        {/* plan title */}
-        <Form.Item {...formItemLayout} label="Designation">
-          {getFieldDecorator('title', {
-            initialValue: isEditForm ? plan.title : undefined,
-            rules: [{ required: true, message: 'Plan time is required' }],
-          })(<Input placeholder="e.g Regional Commissioner" />)}
-        </Form.Item>
-        {/* end plan title */}
-
-        {/* plan abbreviation */}
-        <Form.Item {...formItemLayout} label="Abbreviation">
-          {getFieldDecorator('abbreviation', {
-            initialValue: isEditForm ? plan.abbreviation : undefined,
-          })(<Input placeholder="e.g RC, DC, RAS" />)}
-        </Form.Item>
-        {/* end plan abbreviation */}
-
-        {/* plan number */}
-        <Form.Item {...formItemLayout} label="Phone Number">
-          {getFieldDecorator('mobile', {
-            initialValue: isEditForm ? plan.mobile : undefined,
-            rules: [{ required: true, message: 'Phone number is required' }],
-          })(<Input placeholder="e.g 255799999999" />)}
-        </Form.Item>
-        {/* end plan number */}
-
-        {/* plan email */}
-        <Form.Item {...formItemLayout} label="Email">
-          {getFieldDecorator('email', {
-            initialValue: isEditForm ? plan.email : undefined,
+        {/* plan incident type */}
+        <Form.Item label="Incident Type" {...formItemLayout}>
+          {getFieldDecorator('incidentType', {
             rules: [
               {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
+                required: true,
+                message: 'Please Select Plan Incident Type',
               },
-              { required: true, message: 'Email address is required' },
             ],
-          })(<Input placeholder="e.g example@mail.com" />)}
+            initialValue: isEditForm ? plan.incidentType._id : undefined, //eslint-disable-line
+          })(
+            <SearchableSelectInput
+              placeholder="Select Incident Type ..."
+              onSearch={getIncidentTypes}
+              optionLabel="name"
+              optionValue="_id"
+              initialValue={isEditForm ? plan.incidentType : undefined}
+            />
+          )}
         </Form.Item>
-        {/* end plan email */}
+        {/* end plan incident type */}
+
+        {/* plan owner */}
+        <Form.Item label="Owner" {...formItemLayout}>
+          {getFieldDecorator('owner', {
+            rules: [
+              {
+                required: true,
+                message: 'Please Select the Plan Owner',
+              },
+            ],
+            initialValue: isEditForm ? plan.owner._id : undefined, //eslint-disable-line
+          })(
+            <SearchableSelectInput
+              placeholder="Select Plan Owner ..."
+              onSearch={getStakeholders}
+              optionLabel="name"
+              optionValue="_id"
+              initialValue={isEditForm ? plan.owner : undefined}
+            />
+          )}
+        </Form.Item>
+        {/* end plan owner */}
+
+        {/* plan boundary input */}
+        <Form.Item label="Plan Applicable Area" {...formItemLayout}>
+          {getFieldDecorator('boundary', {
+            rules: [
+              {
+                required: true,
+                message: 'Please Select the Plan Applicable Area',
+              },
+            ],
+            initialValue: isEditForm ? plan.boundary._id : undefined, // eslint-disable-line
+          })(
+            <SearchableSelectInput
+              placeholder="Select Plan Boundary ..."
+              onSearch={getFeatures}
+              optionValue="_id"
+              optionLabel={feature =>
+                `${feature.name} (${upperFirst(feature.type)})`
+              }
+              initialValue={isEditForm ? plan.boundary : undefined}
+            />
+          )}
+        </Form.Item>
+        {/* end plan boundary input */}
 
         {/* form actions */}
         <Form.Item wrapperCol={{ span: 24 }} style={{ textAlign: 'right' }}>
