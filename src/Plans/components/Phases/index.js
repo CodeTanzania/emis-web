764fa69ab1a4555fdfdcf82791238a726a27578e
Benson Maruchu <benmaruchu@gmail.com>
2019-02-11 15:26:45 +0300

    feat(emergency plans): work in progress group activities per phase

---
+++ src/Plans/components/Phases/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * Renders layout of plan activities arranges phase wise
+ *
+ * @function
+ * @name PlanPhases
+ *
+ * @returns {ReactNode}
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+const PlanPhases = () => <div>These are plan activities per phase</div>;
+
+export default PlanPhases;
