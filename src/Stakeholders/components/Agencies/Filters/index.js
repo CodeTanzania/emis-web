c50904f0dafcb4c2c1023c4078c5a98714afd3c8
mloweedgar <mloweedgar0@gmail.com>
2019-02-13 14:44:06 +0300

    feat(stakeholders): add filter to filter agencies

---
+++ src/Stakeholders/components/Agencies/Filters/index.js
@@ -0,0 +1,161 @@
+import {
+  clearStakeholderFilters,
+  Connect,
+  filterStakeholders,
+} from '@codetanzania/emis-api-states';
+import { Button, Checkbox, Col, Form, Row } from 'antd';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+/**
+ * @class
+ * @name AgenciesFilters
+ * @description Filter modal component for filtering agencies
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+class AgenciesFilters extends Component {
+  static propTypes = {
+    filter: PropTypes.objectOf(
+      PropTypes.shape({
+        types: PropTypes.arrayOf(PropTypes.string),
+        phases: PropTypes.arrayOf(PropTypes.string),
+      })
+    ),
+    form: PropTypes.shape({ getFieldDecorator: PropTypes.func }).isRequired,
+    onCancel: PropTypes.func.isRequired,
+    types: PropTypes.arrayOf(PropTypes.string).isRequired,
+    phases: PropTypes.arrayOf(PropTypes.string).isRequired,
+  };
+
+  static defaultProps = {
+    filter: null,
+  };
+
+  /**
+   * @function
+   * @name handleSubmit
+   * @description Handle filter action
+   *
+   * @param {Object} event onSubmit event object
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleSubmit = event => {
+    event.preventDefault();
+    const {
+      form: { validateFields },
+      onCancel,
+    } = this.props;
+
+    validateFields((error, values) => {
+      if (!error) {
+        filterStakeholders(values);
+        onCancel();
+      }
+    });
+  };
+
+  /**
+   * @function
+   * @name handleClearFilter
+   * @description Action handle when clear
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleClearFilter = () => {
+    const { onCancel } = this.props;
+    clearStakeholderFilters();
+    onCancel();
+  };
+
+  render() {
+    const {
+      form: { getFieldDecorator },
+      onCancel,
+      types,
+      phases,
+      filter,
+    } = this.props;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 24 },
+        md: { span: 24 },
+        lg: { span: 24 },
+        xl: { span: 24 },
+        xxl: { span: 24 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 24 },
+        md: { span: 24 },
+        lg: { span: 24 },
+        xl: { span: 24 },
+        xxl: { span: 24 },
+      },
+    };
+
+    return (
+      <Form onSubmit={this.handleSubmit} autoComplete="off">
+        {/* start agency type filters */}
+        <Form.Item {...formItemLayout} label="By Agency type">
+          {getFieldDecorator('type', {
+            initialValue: filter ? filter.type : [],
+          })(
+            <Checkbox.Group style={{ width: '100%' }}>
+              <Row>
+                {types.map(type => (
+                  <Col span={6} style={{ margin: '10px 0' }} key={type}>
+                    <Checkbox value={type}>{type}</Checkbox>
+                  </Col>
+                ))}
+              </Row>
+            </Checkbox.Group>
+          )}
+        </Form.Item>
+        {/* end agency type filters */}
+
+        {/* start emergency phase filters */}
+        <Form.Item {...formItemLayout} label="By Emergency Phases">
+          {getFieldDecorator('phases', {
+            initialValue: filter ? filter.phases : [],
+          })(
+            <Checkbox.Group style={{ width: '100%' }}>
+              <Row>
+                {phases.map(phase => (
+                  <Col span={6} style={{ margin: '10px 0' }} key={phase}>
+                    <Checkbox value={phase}>{phase}</Checkbox>
+                  </Col>
+                ))}
+              </Row>
+            </Checkbox.Group>
+          )}
+        </Form.Item>
+        {/* end emergency phase filters */}
+
+        {/* form actions */}
+        <Form.Item wrapperCol={{ span: 24 }} style={{ textAlign: 'right' }}>
+          <Button onClick={onCancel}>Cancel</Button>
+          <Button style={{ marginLeft: 8 }} onClick={this.handleClearFilter}>
+            Clear
+          </Button>
+          <Button style={{ marginLeft: 8 }} type="primary" htmlType="submit">
+            Filter
+          </Button>
+        </Form.Item>
+        {/* end form actions */}
+      </Form>
+    );
+  }
+}
+
+export default Connect(Form.create()(AgenciesFilters), {
+  types: 'stakeholders.schema.properties.type.enum',
+  phases: 'stakeholders.schema.properties.phases.enum',
+  filter: 'stakeholders.filter',
+});
