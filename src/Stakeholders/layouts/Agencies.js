92f8a59afc8d6aa592f89503e59a2852ad076fc6
mloweedgar <mloweedgar0@gmail.com>
2019-02-13 17:01:08 +0300

    docs(stakeholders): update agency layout documantion
    rename StakeholdersContactsLayout to StakeholdersAgenciesLayout

--- src/Stakeholders/layouts/Agencies.js
+++ src/Stakeholders/layouts/Agencies.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Agencies from '../components/Agencies';
 /**
  * @function
- * @name StakeholdersContactsLayout
+ * @name StakeholdersAgenciesLayout
  * @description Renders Stakeholders Agencies Layout
  *
  * @version 0.1.0
