dc1e03c89163c96d9d5df86acdc0fd5c1372a930
mloweedgar <mloweedgar0@gmail.com>
2019-03-15 12:56:31 +0300

    feat(item categories): display item category item

---
+++ src/Resources/components/ItemCategories/ListItem/index.js
@@ -0,0 +1,170 @@
+import { Avatar, Checkbox, Col, Modal, Row } from 'antd';
+import PropTypes from 'prop-types';
+import randomColor from 'randomcolor';
+import React, { Component } from 'react';
+import ListItemActions from '../../../../components/ListItemActions';
+import './styles.css';
+
+/* constants */
+const { confirm } = Modal;
+
+/**
+ * @class
+ * @name ListItem
+ * @description Single list item component.
+ * Render single item details
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+class ListItem extends Component {
+  state = {
+    isHovered: false,
+  };
+
+  static propTypes = {
+    abbreviation: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    onArchive: PropTypes.func.isRequired,
+    onEdit: PropTypes.func.isRequired,
+    isSelected: PropTypes.bool.isRequired,
+    onSelectItem: PropTypes.func.isRequired,
+    onDeselectItem: PropTypes.func.isRequired,
+    onShare: PropTypes.func.isRequired,
+  };
+
+  /**
+   * @function
+   * @name handleMouseEnter
+   * @description Handle on MouseEnter ListItem event
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleMouseEnter = () => {
+    this.setState({ isHovered: true });
+  };
+
+  /**
+   * @function
+   * @name handleMouseEnter
+   * @description Handle on MouseLeave ListItem event
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleMouseLeave = () => {
+    this.setState({ isHovered: false });
+  };
+
+  /**
+   * @function
+   * @name handleToggleSelect
+   * @description Handle Toggling List ItemCategory checkbox
+   *
+   * @param {Object} event - Event object
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  handleToggleSelect = event => {
+    const { isSelected } = this.state;
+    const { onSelectItem, onDeselectItem } = this.props;
+
+    this.setState({ isSelected: !isSelected });
+    if (event.target.checked) {
+      onSelectItem();
+    } else {
+      onDeselectItem();
+    }
+  };
+
+  /**
+   * @function
+   * @name showArchiveConfirm
+   * @description show confirm modal before archiving a item
+   *
+   * @version 0.1.0
+   * @since 0.1.0
+   */
+  showArchiveConfirm = () => {
+    const { name, onArchive } = this.props;
+    confirm({
+      title: `Are you sure you want to archive ${name} ?`,
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        onArchive();
+      },
+    });
+  };
+
+  render() {
+    const { abbreviation, description, name, onEdit, onShare } = this.props;
+    const { isHovered } = this.state;
+    const { isSelected } = this.props;
+    const avatarBackground = randomColor();
+    let sideComponent = null;
+
+    if (isSelected) {
+      sideComponent = (
+        <Checkbox
+          className="Checkbox"
+          onChange={this.handleToggleSelect}
+          checked={isSelected}
+        />
+      );
+    } else {
+      sideComponent = isHovered ? (
+        <Checkbox
+          className="Checkbox"
+          onChange={this.handleToggleSelect}
+          checked={isSelected}
+        />
+      ) : (
+        <Avatar style={{ backgroundColor: avatarBackground }}>
+          {abbreviation}
+        </Avatar>
+      );
+    }
+
+    return (
+      <div
+        className="ListItem"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
+        <Row>
+          <Col span={1}>{sideComponent}</Col>
+          <Col span={6}>{name}</Col>
+          <Col span={14}>{description}</Col>
+          <Col span={1}>
+            {isHovered && (
+              <ListItemActions
+                edit={{
+                  name: 'Edit ItemCategory',
+                  title: 'Update ItemCategory Details',
+                  onClick: onEdit,
+                }}
+                share={{
+                  name: 'Share ItemCategory',
+                  title: 'Share ItemCategory details with others',
+                  onClick: onShare,
+                }}
+                archive={{
+                  name: 'Archive ItemCategory',
+                  title: 'Remove ItemCategory from list of active items',
+                  onClick: this.showArchiveConfirm,
+                }}
+              />
+            )}
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default ListItem;
