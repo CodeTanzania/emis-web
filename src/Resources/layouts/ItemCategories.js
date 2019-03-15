9ecca72f8fc4b4b59f2c88d7de9885a69a1a7545
mloweedgar <mloweedgar0@gmail.com>
2019-03-15 10:41:44 +0300

    feat(item categories): implement layout for item categories

---
+++ src/Resources/layouts/ItemCategories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import UIState from '../../components/UIState';
+import ItemCategories from '../components/ItemCategories';
+
+/**
+ * @function
+ * @name ResourceItemCategoriesLayout
+ * @description Render resources Item Categories layout
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+const ResourceItemCategoriesLayout = () => <ItemCategories />;
+
+export default ResourceItemCategoriesLayout;
