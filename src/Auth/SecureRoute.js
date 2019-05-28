af4728b62f138d81a5e9010777711bdebf02c838
Benson Maruchu <benmaruchu@gmail.com>
2019-05-28 15:04:20 +0300

    feat(auth): add secure route component

---
+++ src/Auth/SecureRoute.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+/**
+ * @function
+ * @name SecureRoute
+ * @description Route which check authentication status and route to appropiate component
+ *
+ * @param {object} props  React props
+ * @param {object} props.Component Component to be rendered
+ *
+ * @returns {object} React Element
+ */
+const SecureRoute = ({ Component, ...rest }) => {
+  const isAuthenticated = true;
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: { from: props.location } }} // eslint-disable-line
+          />
+        )
+      }
+    />
+  );
+};
+
+/* props validation */
+SecureRoute.propTypes = {
+  Component: PropTypes.node.isRequired,
+};
+
+export default SecureRoute;
