b8df452c2a60996efa274b62c5bdc5c4f3c3cad9
Benson Maruchu <benmaruchu@gmail.com>
2019-05-28 15:56:14 +0300

    fix(auth): add navigation to login and base layout components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { initializeApp, StoreProvider } from '@codetanzania/emis-api-states';
 import { Icon, Spin } from 'antd';
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import BaseLayout from './layouts/BaseLayout';
 
 // populate app store with schemas
@@ -13,7 +13,10 @@ Spin.setDefaultIndicator(<Icon type="loading" style={{ fontSize: 24 }} spin />);
 const App = () => (
   <StoreProvider>
     <HashRouter hashType="hashbang">
-      <BaseLayout />
+      <Switch>
+        <Route exact path="/" component={BaseLayout} />
+        {/* <Route path="/login" component={} /> */}
+      </Switch>
     </HashRouter>
   </StoreProvider>
 );
