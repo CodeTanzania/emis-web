78c4e66cc98bda56d3471b0018d098d00d254530
mloweedgar <mloweedgar0@gmail.com>
2019-06-18 12:17:24 +0300

    refactor(alerts): update alert ui
    update alerts ui to resemble stardard emis module ui just like stakeholders ui

--- src/Alerts/components/Alerts/Filters/index.js
+++ src/Alerts/components/Alerts/Filters/index.js
@@ -66,7 +66,7 @@ class AlertsFilters extends Component {
    * @name handleSubmit
    * @description Handle filter action
    *
-   * @param {Object} e event object
+   * @param {object} e event object
    *
    * @version 0.1.0
    * @since 0.1.0
