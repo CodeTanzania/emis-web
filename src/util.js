68736dfdf88a1bd26298eafff8e70e720f703e3a
Benson Maruchu <benmaruchu@gmail.com>
2019-07-15 16:13:59 +0300

    feat(util): add rgba color generator function

--- src/util.js
+++ src/util.js
@@ -92,3 +92,30 @@ export const timeAgo = date => moment(date).fromNow();
  */
 export const formatNumber = number =>
   new Intl.NumberFormat('en-US').format(number);
+
+/**
+ * @function
+ * @name getRGBAColor
+ * @description Return RGBA color from base color and alpha value
+ *
+ * @param {string} baseColor  Base color i.e #ffddee
+ * @param {number} alpha Alpha value should be between 0 and 1
+ *
+ * @returns {string} rbga(r,b,g,a)
+ *
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+export const getRGBAColor = (baseColor, alpha) => {
+  const values = baseColor.split('');
+
+  if (values.length < 7 || alpha > 1 || alpha < 0) {
+    return undefined;
+  }
+
+  const r = parseInt(`0x${values[1]}${values[2]}`, 16);
+  const g = parseInt(`0x${values[3]}${values[4]}`, 16);
+  const b = parseInt(`0x${values[5]}${values[6]}`, 16);
+
+  return `rgba(${r},${g},${b},${alpha})`;
+};
