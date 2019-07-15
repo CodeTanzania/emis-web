725f8c03922619303236dc727f152e355c2500a6
Benson Maruchu <benmaruchu@gmail.com>
2019-07-15 12:43:36 +0300

    feat(dashboards): redesign overview dashboard to have map only

--- src/Dashboards/index.js
+++ src/Dashboards/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Divider, Row, Col, Card, Icon, Statistic, Tooltip } from 'antd';
+import React, { useState } from 'react';
+import { Tooltip, Row, Col, Card, Typography } from 'antd';
 import {
   ComposableMap,
   Geographies,
@@ -7,7 +7,12 @@ import {
   ZoomableGroup,
 } from 'react-simple-maps';
 import randomColor from 'randomcolor';
+import { formatNumber } from '../util';
 import DarWards from '../assets/maps/dar.wards.json';
+
+/* declarations */
+
+const { Text } = Typography;
 
 /**
  * @function
@@ -20,162 +25,197 @@ import DarWards from '../assets/maps/dar.wards.json';
  * @since 0.1.0
  */
 const OverviewDashboard = () => {
+  const [ward, setWard] = useState(null);
+
   return (
     <div>
-      {/* stakeholder section */}
-      <Divider orientation="left">Stakeholders</Divider>
       <Row>
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Active Focal People"
-              value={50}
-              prefix={<Icon type="team" style={{ color: '#2C90FF' }} />}
-            />
-          </Card>
-        </Col>
+        <Col span={16}>
+          {/* ward svg map */}
+          <ComposableMap
+            projectionConfig={{
+              scale: 90000,
+              xOffset: -70,
+              yOffset: -50,
+            }}
+            width={800}
+            height={850}
+            style={{
+              margin: '10px 50px',
+            }}
+          >
+            <ZoomableGroup center={[39.1037144, -6.7923668]}>
+              <Geographies geography={DarWards}>
+                {(geographies, projection) =>
+                  geographies.map(geography => {
+                    // console.log(geography.properties.Female_Pop);
+                    // console.log(geography.properties.Male_Pop);
 
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Agencies"
-              value={20}
-              prefix={<Icon type="cluster" style={{ color: '#2C90FF' }} />}
-            />
-          </Card>
-        </Col>
+                    const defaultColor = randomColor({
+                      hue: '#00ffff',
+                      luminosity: 'light',
+                    });
+                    const hoverColor = randomColor({
+                      hue: '#00ffff',
+                      luminosity: 'dark',
+                    });
+                    const pressedColor = randomColor({ hue: '#00ffff' });
 
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Notifications Sent"
-              value={3000}
-              prefix={<Icon type="mail" style={{ color: '#2C90FF' }} />}
-            />
-          </Card>
+                    return (
+                      <Tooltip
+                        key={geography.properties.fid}
+                        trigger="hover"
+                        title={geography.properties.Ward_Name}
+                      >
+                        <Geography
+                          key={geography.properties.fid}
+                          geography={geography}
+                          projection={projection}
+                          onClick={() => setWard(geography.properties)}
+                          style={{
+                            default: {
+                              fill: defaultColor,
+                              stroke: '#607D8B',
+                              strokeWidth: 0.75,
+                              outline: 'none',
+                            },
+                            hover: {
+                              fill: hoverColor,
+                              stroke: '#607D8B',
+                              strokeWidth: 0.75,
+                              outline: 'none',
+                            },
+                            pressed: {
+                              fill: pressedColor,
+                              stroke: '#607D8B',
+                              strokeWidth: 0.75,
+                              outline: 'none',
+                            },
+                          }}
+                        />
+                      </Tooltip>
+                    );
+                  })
+                }
+              </Geographies>
+            </ZoomableGroup>
+          </ComposableMap>
+          {/* end ward svg map */}
         </Col>
+        <Col span={8}>
+          <div style={{ margin: '20px 10px' }}>
+            <Card title="Ward Summary">
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Ward : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? ward.Ward_Name : 'N/A'}</Col>
+                </Row>
+              </p>
 
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Notifications Delivery Rate"
-              value={70}
-              prefix={<Icon type="check" style={{ color: '#52C41A' }} />}
-              suffix="%"
-            />
-          </Card>
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> District : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? ward.District_N : 'N/A'}</Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Population : </Text>
+                  </Col>
+                  <Col span={12}>
+                    {' '}
+                    {ward ? formatNumber(ward.Ward_Pop) : 'N/A'}
+                  </Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Male Population : </Text>
+                  </Col>
+                  <Col span={12}>
+                    {' '}
+                    {ward ? formatNumber(ward.Male_Pop) : 'N/A'}
+                  </Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Female Population : </Text>
+                  </Col>
+                  <Col span={12}>
+                    {' '}
+                    {ward ? formatNumber(ward.Female_Pop) : 'N/A'}
+                  </Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Below 10 Years Population : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? 'N/A' : 'N/A'}</Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Above 65 Years Population : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? 'N/A' : 'N/A'}</Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Evacuation Centers : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? 'N/A' : 'N/A'}</Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Health Centers : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? 'N/A' : 'N/A'}</Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Households at risk : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? 'N/A' : 'N/A'}</Col>
+                </Row>
+              </p>
+
+              <p>
+                <Row>
+                  <Col span={12}>
+                    <Text strong> Focal Person : </Text>
+                  </Col>
+                  <Col span={12}> {ward ? 'N/A' : 'N/A'}</Col>
+                </Row>
+              </p>
+            </Card>
+          </div>
         </Col>
       </Row>
-
-      {/* ward svg map */}
-      <ComposableMap
-        projectionConfig={{
-          scale: 70000,
-          xOffset: -70,
-          yOffset: -50,
-        }}
-        width={800}
-        height={850}
-        style={{
-          border: '1px solid #dedede',
-          margin: '10px 30px',
-        }}
-      >
-        <ZoomableGroup center={[39.1037144, -6.7923668]}>
-          <Geographies geography={DarWards}>
-            {(geographies, projection) =>
-              geographies.map(geography => {
-                // console.log(geography.properties.Female_Pop);
-                // console.log(geography.properties.Male_Pop);
-                // console.log(geography.properties.Ward_Pop);
-                const defaultColor = randomColor({
-                  hue: '#00ffff',
-                  luminosity: 'light',
-                });
-                const hoverColor = randomColor({
-                  hue: '#00ffff',
-                  luminosity: 'dark',
-                });
-                const pressedColor = randomColor({ hue: '#00ffff' });
-
-                return (
-                  <Tooltip
-                    key={geography.properties.fid}
-                    trigger="hover"
-                    title={geography.properties.Ward_Name}
-                  >
-                    <Geography
-                      key={geography.properties.fid}
-                      geography={geography}
-                      projection={projection}
-                      style={{
-                        default: {
-                          fill: defaultColor,
-                          stroke: '#607D8B',
-                          strokeWidth: 0.75,
-                          outline: 'none',
-                        },
-                        hover: {
-                          fill: hoverColor,
-                          stroke: '#607D8B',
-                          strokeWidth: 0.75,
-                          outline: 'none',
-                        },
-                        pressed: {
-                          fill: pressedColor,
-                          stroke: '#607D8B',
-                          strokeWidth: 0.75,
-                          outline: 'none',
-                        },
-                      }}
-                    />
-                  </Tooltip>
-                );
-              })
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-      {/* end ward svg map */}
-
-      {/* end stakeholder section */}
-
-      {/* Resource section */}
-      <Divider orientation="left">Resources</Divider>
-      <Row>
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Warehouses"
-              value={50}
-              prefix={<Icon type="bank" style={{ color: '#2C90FF' }} />}
-            />
-          </Card>
-        </Col>
-
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Items in Stock"
-              value={20}
-              prefix={<Icon type="container" style={{ color: '#2C90FF' }} />}
-            />
-          </Card>
-        </Col>
-
-        <Col span={4} offset={1}>
-          <Card>
-            <Statistic
-              title="Items out of Stock"
-              value={4}
-              prefix={
-                <Icon type="exclamation-circle" style={{ color: '#CF1321' }} />
-              }
-            />
-          </Card>
-        </Col>
-      </Row>
-      {/* end resource section */}
     </div>
   );
 };
